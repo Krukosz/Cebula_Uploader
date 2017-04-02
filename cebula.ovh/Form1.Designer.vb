a78ac11574fa70ec1486fdfc7672bf5657bc7a99
Krukosz <lukasz.krukowski@student.put.poznan.pl>
2017-04-02 16:10:42 +0200

    1.00
    First stable version.

---
+++ cebula.ovh/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.LB_Pending = New System.Windows.Forms.ListBox()
+        Me.Lbl1 = New System.Windows.Forms.Label()
+        Me.btn_SelectFile = New System.Windows.Forms.Button()
+        Me.btn_Upload = New System.Windows.Forms.Button()
+        Me.LB_Uploaded = New System.Windows.Forms.ListBox()
+        Me.btn_Clear = New System.Windows.Forms.Button()
+        Me.Lbl2 = New System.Windows.Forms.Label()
+        Me.btn_Copy = New System.Windows.Forms.Button()
+        Me.myBrowser = New System.Windows.Forms.WebBrowser()
+        Me.lbl_Status = New System.Windows.Forms.Label()
+        Me.ProgBar_Status = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Multiselect = True
+        Me.OpenFileDialog1.Title = "Select files to upload..."
+        '
+        'LB_Pending
+        '
+        Me.LB_Pending.FormattingEnabled = True
+        Me.LB_Pending.Location = New System.Drawing.Point(15, 29)
+        Me.LB_Pending.Name = "LB_Pending"
+        Me.LB_Pending.Size = New System.Drawing.Size(400, 69)
+        Me.LB_Pending.TabIndex = 0
+        '
+        'Lbl1
+        '
+        Me.Lbl1.AutoSize = True
+        Me.Lbl1.Location = New System.Drawing.Point(12, 13)
+        Me.Lbl1.Name = "Lbl1"
+        Me.Lbl1.Size = New System.Drawing.Size(78, 13)
+        Me.Lbl1.TabIndex = 1
+        Me.Lbl1.Text = "Files to upload:"
+        '
+        'btn_SelectFile
+        '
+        Me.btn_SelectFile.Location = New System.Drawing.Point(421, 29)
+        Me.btn_SelectFile.Name = "btn_SelectFile"
+        Me.btn_SelectFile.Size = New System.Drawing.Size(100, 30)
+        Me.btn_SelectFile.TabIndex = 2
+        Me.btn_SelectFile.Text = "Select files..."
+        Me.btn_SelectFile.UseVisualStyleBackColor = True
+        '
+        'btn_Upload
+        '
+        Me.btn_Upload.Location = New System.Drawing.Point(421, 156)
+        Me.btn_Upload.Name = "btn_Upload"
+        Me.btn_Upload.Size = New System.Drawing.Size(100, 30)
+        Me.btn_Upload.TabIndex = 3
+        Me.btn_Upload.Text = "UPLOAD"
+        Me.btn_Upload.UseVisualStyleBackColor = True
+        '
+        'LB_Uploaded
+        '
+        Me.LB_Uploaded.FormattingEnabled = True
+        Me.LB_Uploaded.Location = New System.Drawing.Point(15, 117)
+        Me.LB_Uploaded.Name = "LB_Uploaded"
+        Me.LB_Uploaded.Size = New System.Drawing.Size(400, 69)
+        Me.LB_Uploaded.TabIndex = 4
+        '
+        'btn_Clear
+        '
+        Me.btn_Clear.Location = New System.Drawing.Point(421, 68)
+        Me.btn_Clear.Name = "btn_Clear"
+        Me.btn_Clear.Size = New System.Drawing.Size(100, 30)
+        Me.btn_Clear.TabIndex = 5
+        Me.btn_Clear.Text = "Clear"
+        Me.btn_Clear.UseVisualStyleBackColor = True
+        '
+        'Lbl2
+        '
+        Me.Lbl2.AutoSize = True
+        Me.Lbl2.Location = New System.Drawing.Point(12, 101)
+        Me.Lbl2.Name = "Lbl2"
+        Me.Lbl2.Size = New System.Drawing.Size(77, 13)
+        Me.Lbl2.TabIndex = 6
+        Me.Lbl2.Text = "Uploaded files:"
+        '
+        'btn_Copy
+        '
+        Me.btn_Copy.Location = New System.Drawing.Point(421, 117)
+        Me.btn_Copy.Name = "btn_Copy"
+        Me.btn_Copy.Size = New System.Drawing.Size(100, 30)
+        Me.btn_Copy.TabIndex = 7
+        Me.btn_Copy.Text = "Copy to Clipboard"
+        Me.btn_Copy.UseVisualStyleBackColor = True
+        '
+        'myBrowser
+        '
+        Me.myBrowser.Location = New System.Drawing.Point(15, 29)
+        Me.myBrowser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.myBrowser.Name = "myBrowser"
+        Me.myBrowser.Size = New System.Drawing.Size(400, 69)
+        Me.myBrowser.TabIndex = 8
+        Me.myBrowser.WebBrowserShortcutsEnabled = False
+        '
+        'lbl_Status
+        '
+        Me.lbl_Status.AutoSize = True
+        Me.lbl_Status.Location = New System.Drawing.Point(12, 189)
+        Me.lbl_Status.Name = "lbl_Status"
+        Me.lbl_Status.Size = New System.Drawing.Size(74, 13)
+        Me.lbl_Status.TabIndex = 9
+        Me.lbl_Status.Text = "Status: Ready"
+        '
+        'ProgBar_Status
+        '
+        Me.ProgBar_Status.Location = New System.Drawing.Point(15, 205)
+        Me.ProgBar_Status.Name = "ProgBar_Status"
+        Me.ProgBar_Status.Size = New System.Drawing.Size(506, 23)
+        Me.ProgBar_Status.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(544, 240)
+        Me.Controls.Add(Me.ProgBar_Status)
+        Me.Controls.Add(Me.lbl_Status)
+        Me.Controls.Add(Me.btn_Copy)
+        Me.Controls.Add(Me.Lbl2)
+        Me.Controls.Add(Me.btn_Clear)
+        Me.Controls.Add(Me.LB_Uploaded)
+        Me.Controls.Add(Me.btn_Upload)
+        Me.Controls.Add(Me.btn_SelectFile)
+        Me.Controls.Add(Me.Lbl1)
+        Me.Controls.Add(Me.LB_Pending)
+        Me.Controls.Add(Me.myBrowser)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "cebula.ovh :: Uploader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents LB_Pending As ListBox
+    Friend WithEvents Lbl1 As Label
+    Friend WithEvents btn_SelectFile As Button
+    Friend WithEvents btn_Upload As Button
+    Friend WithEvents LB_Uploaded As ListBox
+    Friend WithEvents btn_Clear As Button
+    Friend WithEvents Lbl2 As Label
+    Friend WithEvents btn_Copy As Button
+    Friend WithEvents myBrowser As WebBrowser
+    Friend WithEvents lbl_Status As Label
+    Friend WithEvents ProgBar_Status As ProgressBar
+End Class
---
+++ cebula.ovh/Form1.vb
@@ -0,0 +1,306 @@
+﻿Imports System.Threading
+Imports System.Text.RegularExpressions
+
+Module InvokeWebBrowser
+
+    <Runtime.CompilerServices.Extension()>
+    Public Sub InvokeCustom(ByVal Control As Control, ByVal Action As Action)
+        If Control.InvokeRequired Then Control.Invoke(New MethodInvoker(Sub() Action()), Nothing) Else Action.Invoke()
+    End Sub
+
+End Module
+
+Public Class Form1
+    Declare Sub Sleep Lib "kernel32.dll" (ByVal Milliseconds As Integer)
+
+    Private ThreadBrowse, ThreadAutoMode As Thread
+    Dim aTimer As Timers.Timer
+    Private _WaitHandle_FirstThreadDone As New System.Threading.AutoResetEvent(False)
+
+    Public QuietMode As Boolean = False
+    Public PubFilePath As String = ""
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub StartNavigate()
+        ThreadBrowse = New Thread(AddressOf Cebula_Upload)
+        ThreadBrowse.Name = "StartNavigate"
+        ThreadBrowse.SetApartmentState(ApartmentState.STA)
+
+        ThreadBrowse.Start()
+        'ThreadBrowse.Join()
+        'Return
+    End Sub
+    Function GetFileName(FullFilePath As String) As String
+        Dim FileName As String = ""
+        Dim SplitStr As String()
+
+        If InStr(FullFilePath, "\") Then
+            SplitStr = Split(FullFilePath, "\")
+            FileName = SplitStr(UBound(SplitStr))
+        Else
+            FileName = FullFilePath
+        End If
+
+        Return FileName
+    End Function
+    Private Sub Cebula_Upload()
+        Me.Invoke(DirectCast(Sub() ProgBar_Status.Value = ProgBar_Status.Minimum, MethodInvoker))
+        Dim IsFinish As Boolean = False
+        Dim WebBro As WebBrowser = myBrowser
+        Dim address As String = "http://cebula.ovh/upload/index.php"
+
+        WebBro.AllowNavigation = True
+        WebBro.Navigate(New Uri(address))
+
+        Do Until IsFinish = True
+            Thread.Sleep(100)
+            WebBro.InvokeCustom(Sub() IsFinish = (WebBro.ReadyState = WebBrowserReadyState.Complete))
+        Loop
+        Me.Invoke(DirectCast(Sub() lbl_Status.Text = "Status: Connected to website.", MethodInvoker))
+        Dim getCebula As HtmlElementCollection = Nothing
+        Dim foundInputFile As Integer = -1
+        Dim foundInputSend As Integer = -1
+        Dim foundInputLink As Integer = -1
+        Dim uploadedLink As String
+
+
+        Dim totalfilesInt As Integer = 0
+        Dim totalfilesOut As Integer = 0
+        Dim percentageBar As Double
+        Me.Invoke(DirectCast(Sub() totalfilesInt = LB_Pending.Items.Count, MethodInvoker))
+
+        Dim shownFileName As String
+
+
+        For j = 0 To totalfilesInt - 1
+            Me.Invoke(DirectCast(Sub() PubFilePath = LB_Pending.Items(j).ToString, MethodInvoker))
+            shownFileName = GetFileName(PubFilePath)
+            Me.Invoke(DirectCast(Sub() lbl_Status.Text = "Status: Uploading file " & shownFileName, MethodInvoker))
+
+            Do Until IsFinish = True
+                Thread.Sleep(100)
+                WebBro.InvokeCustom(Sub() IsFinish = (WebBro.ReadyState = WebBrowserReadyState.Complete))
+            Loop
+
+            getCebula = Nothing
+
+            Do While IsNothing(getCebula)
+                Thread.Sleep(100)
+                WebBro.InvokeCustom(Sub() getCebula = WebBro.Document.GetElementsByTagName("input"))
+            Loop
+
+            For i = 0 To getCebula.Count - 1
+
+                If getCebula(i).GetAttribute("type") = "file" Then
+                    foundInputFile = i
+                End If
+                If getCebula(i).GetAttribute("type") = "submit" Then
+                    foundInputSend = i
+                End If
+            Next
+
+            aTimer = New Timers.Timer
+            AddHandler aTimer.Elapsed, AddressOf OnTimedEvent
+            aTimer.Interval = 1000
+            aTimer.Enabled = True
+
+            getCebula(foundInputFile).InvokeMember("Click")
+            aTimer.Enabled = False
+
+            getCebula(foundInputSend).InvokeMember("Click")
+            Application.DoEvents()
+            Thread.Sleep(500)
+            IsFinish = False
+            Dim isReloading = True
+
+            Do Until isReloading = False
+                Thread.Sleep(100)
+                WebBro.InvokeCustom(Sub() isReloading = WebBro.IsBusy)
+            Loop
+
+            Do Until IsFinish = True
+                Thread.Sleep(100)
+                WebBro.InvokeCustom(Sub() IsFinish = (WebBro.ReadyState = WebBrowserReadyState.Complete))
+            Loop
+
+            getCebula = Nothing
+
+            Do While IsNothing(getCebula)
+                Thread.Sleep(100)
+                WebBro.InvokeCustom(Sub() getCebula = WebBro.Document.GetElementsByTagName("input"))
+            Loop
+
+            For k = 0 To getCebula.Count - 1
+
+                If getCebula(k).GetAttribute("type") = "text" Then
+                    foundInputLink = k
+                End If
+            Next
+
+            uploadedLink = getCebula(foundInputLink).GetAttribute("value")
+            Me.Invoke(DirectCast(Function() LB_Uploaded.Items.Add(uploadedLink), MethodInvoker))
+
+            Me.Invoke(DirectCast(Sub() totalfilesOut = LB_Uploaded.Items.Count, MethodInvoker))
+
+            percentageBar = (totalfilesOut / totalfilesInt) * 100
+            Me.Invoke(DirectCast(Sub() ProgBar_Status.Value = percentageBar, MethodInvoker))
+            'Thread.Sleep(1000)
+        Next
+        Me.Invoke(DirectCast(Sub() ProgBar_Status.Value = ProgBar_Status.Maximum, MethodInvoker))
+        Me.Invoke(DirectCast(Sub() lbl_Status.Text = "Status: Complete", MethodInvoker))
+
+        _WaitHandle_FirstThreadDone.Set()
+    End Sub
+    Private Sub btn_SelectFile_Click(sender As Object, e As EventArgs) Handles btn_SelectFile.Click
+        OpenFileDialog1.ShowDialog()
+
+        Dim selectedFiles As String() = OpenFileDialog1.FileNames
+
+        If Not IsNothing(selectedFiles) Then
+            'Cos wybrane
+            For i = LBound(selectedFiles) To UBound(selectedFiles)
+                LB_Pending.Items.Add(selectedFiles(i))
+            Next
+        End If
+    End Sub
+
+    Private Sub btn_Clear_Click(sender As Object, e As EventArgs) Handles btn_Clear.Click
+        LB_Pending.Items.Clear()
+        LB_Uploaded.Items.Clear()
+        ProgBar_Status.Value = ProgBar_Status.Minimum
+        lbl_Status.Text = "Status: Ready"
+    End Sub
+
+    Private Sub btn_Upload_Click(sender As Object, e As EventArgs) Handles btn_Upload.Click
+        'Prog_Bar_1.Enabled = True
+        'Prog_Bar_1.Update()
+        Call StartNavigate()
+        'ThreadBrowse.Join()
+    End Sub
+
+    Private Sub OnTimedEvent(source As Object, e As Timers.ElapsedEventArgs)
+        aTimer.Stop()
+
+        SendKeys.SendWait(PubFilePath) 'Fill in the full file path and name you want to open
+        SendKeys.SendWait("{TAB 2}") 'Move focus to the Open button
+        SendKeys.SendWait("{ENTER}") 'Press the Open button
+        'Return
+    End Sub
+
+    Private Sub btn_Copy_Click(sender As Object, e As EventArgs) Handles btn_Copy.Click
+        Call CopyLinks()
+    End Sub
+
+    Public Sub CopyLinks()
+        Dim links As String
+        For i = 0 To LB_Uploaded.Items.Count - 1
+            links &= LB_Uploaded.Items(i).ToString & vbCrLf
+        Next
+        links = Trim(links)
+        Clipboard.SetText(links)
+    End Sub
+
+    Public Function extractFilePath(rawArgs() As String) As String()
+        Dim pathpattern As String
+        pathpattern = "^(?:[a-zA-Z]\:(\\|\/)|file\:\/\/|\\\\|\.(\/|\\))([^\\\/\:\*\?\<\>\""\|]+(\\|\/){0,1})+$"
+        Dim matches As Integer = 0
+
+        Dim filesArr As String()
+        ReDim filesArr(0) '1-element array
+        For i = 0 To UBound(rawArgs)
+            If Regex.IsMatch(rawArgs(i), pathpattern) Then
+                matches += 1
+                If matches > filesArr.Length Then
+                    ReDim Preserve filesArr(matches - 1)
+                    filesArr(matches - 1) = rawArgs(i)
+                Else
+                    filesArr(matches - 1) = rawArgs(i)
+                End If
+            End If
+        Next
+
+        If filesArr(0) = "" Then
+            filesArr(0) = "ERROR"
+        End If
+
+        Return filesArr
+    End Function
+
+    Public Sub AutoModeProcess()
+        Call StartNavigate()
+        _WaitHandle_FirstThreadDone.WaitOne()
+        Call CopyLinks()
+        Application.Exit()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '/a - auto mode - gets files, uploads & copies to clipboard & exit
+        '/q - quiet mode - nie wiem czy sie uda
+        Dim AutoMode As Boolean = False
+        'Dim QuietMode As Boolean = False
+        Dim execArgs As String()
+        Dim apppath As String
+        apppath = Application.ExecutablePath
+        'MsgBox(apppath)
+        execArgs = Environment.GetCommandLineArgs
+        Dim filepathArgs As String()
+        filepathArgs = extractFilePath(execArgs)
+
+        For j = 0 To execArgs.Length - 1
+            If execArgs(j) = "/a" Then
+                AutoMode = True
+            ElseIf execArgs(j) = "/q" Then
+                QuietMode = True
+                Me.ShowInTaskbar = False
+                'Me.WindowState = 1
+            End If
+        Next
+
+        For i = 0 To filepathArgs.Length - 1
+            If filepathArgs(i).ToString <> "ERROR" Then
+                If filepathArgs(i).ToString <> apppath.ToString Then
+                    LB_Pending.Items.Add(filepathArgs(i).ToString)
+                End If
+            End If
+        Next
+
+        'Dim text As String
+        'For i = 0 To UBound(filepathArgs)
+        '    text &= filepathArgs(i).ToString
+        'Next
+
+        If AutoMode = True Then
+            'MsgBox(filepathArgs.Length.ToString)
+            'MsgBox(text)
+            If filepathArgs.Length > 1 Then
+
+                ThreadAutoMode = New Thread(AddressOf AutoModeProcess)
+                ThreadAutoMode.Name = "AutoModeThread"
+                ThreadAutoMode.SetApartmentState(ApartmentState.STA)
+
+                ThreadAutoMode.Start()
+            Else
+                MsgBox("Cannot find files to upload, add file paths in startup command." & vbCrLf & "Application will close.", vbOKOnly, "Missing file parameter")
+                Application.Exit()
+            End If
+        ElseIf QuietMode = True And AutoMode = False
+            MsgBox("Quiet parameter can be used in auto-mode only." & vbCrLf & "Application will close.", vbOKOnly, "Wrong startup parameters")
+            Application.Exit()
+        End If
+
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        If QuietMode = True Then
+            Me.Hide()
+        End If
+    End Sub
+End Class
